fa820786c9ec21ba224653be76792cc8fa16c498
danimor99 <danimor1999@gmail.com>
2024-11-27 20:45:39 +0100

    Impresion argumentos

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main{
 	public static void main (String[] args){
 		System.out.println("Hola mundo");
+
+		if(ags.length() == 0){
+			System.out.println("No hay argumentos");
+		}
+		else
+			System.out.println("Hay " + args.length() + " argumentos");
+		}
 	}
 }
